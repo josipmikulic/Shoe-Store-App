4d7c014520586667aff132f4d6aeb1b2d39ab011
josip <josip.mikulic@thefootballcompany.com>
2022-06-12 14:32:53 +0200

    * Review fix

--- app/src/main/java/com/udacity/shoestore/ui/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeViewModel.kt
@@ -8,43 +8,19 @@ import com.udacity.shoestore.models.Shoe
 class ShoeViewModel : ViewModel() {
 
     private val _shoeData = mutableListOf<Shoe>()
-    private val _name = MutableLiveData("")
-    private val _company = MutableLiveData("")
-    private val _size = MutableLiveData(0)
-    private val _description = MutableLiveData("")
+    val _name = MutableLiveData("")
+    val _company = MutableLiveData("")
+    val _size = MutableLiveData("")
+    val _description = MutableLiveData("")
 
     val shoeData: List<Shoe> = _shoeData
     val name: LiveData<String> = _name
     val company: LiveData<String> = _company
-    val size: LiveData<Int> = _size
+    val size: LiveData<String> = _size
     val description: LiveData<String> = _description
 
-    fun onNameChanged(name: CharSequence) {
-        if (_name.value != name.toString()) {
-            _name.value = name.toString()
-        }
-    }
-
-    fun onCompanyChanged(company: CharSequence) {
-        if (_company.value != company.toString()) {
-            _company.value = company.toString()
-        }
-    }
-
-    fun onSizeChanged(size: CharSequence) {
-        if (_size.value != size.toString().toInt()) {
-            _size.value = size.toString().toInt()
-        }
-    }
-
-    fun onDescriptionChanged(description: CharSequence) {
-        if (_description.value != description.toString()) {
-            _description.value = description.toString()
-        }
-    }
-
     fun saveShoeData() {
-        val newShoe = Shoe(name.value, company.value, size.value, description.value)
+        val newShoe = Shoe(name.value, company.value, size.value?.toInt(), description.value)
         _shoeData.add(newShoe)
     }
 }
